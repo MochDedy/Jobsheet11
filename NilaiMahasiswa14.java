3d1b234508b71a6a6005cf59b8f8862887373928
MochDedy <mochdedy96@gmail.com>
2025-12-05 12:32:20 +0700

    Tugas2

---
+++ NilaiMahasiswa14.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class NilaiMahasiswa14 {
+    static void isianArray(int[] nilai) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai[i] = input.nextInt();
+        }
+    }
+
+    static void tampilArray(int[] nilai) {
+        System.out.println("\nDaftar Nilai Mahasiswa:");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Mahasiswa ke-" + (i + 1) + ": " + nilai[i]);
+        }
+    }
+
+    static int hitTot(int[] nilai) {
+        int total = 0;
+        for (int i = 0; i < nilai.length; i++) {
+            total += nilai[i];
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa (N): ");
+        int N = input.nextInt();
+
+        int[] nilaiMhs = new int[N];
+        isianArray(nilaiMhs);
+        tampilArray(nilaiMhs);
+
+        int totalNilai = hitTot(nilaiMhs);
+
+        System.out.println("\nTotal nilai seluruh mahasiswa: " + totalNilai);
+
+        input.close();
+    }
+}
